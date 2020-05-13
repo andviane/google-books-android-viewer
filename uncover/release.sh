4dec54e57c95a3c3d02a06e533151d5c364ce920
audrius.meskauskas <audrius.meskauskas@meska.info>
2020-05-13 11:26:21 +0200

    Make buildable with the new Android version

---
+++ uncover/release.sh
@@ -0,0 +1,4 @@
+mkdir -p /home/audrius/AndroidStudioProjects/BookList/uncover/build/publications/maven/
+touch  /home/audrius/AndroidStudioProjects/BookList/uncover/build/publications/maven/pom-default.xml
+../gradlew assembleRelease signDefault signArchives pu
+
